61ec6cf63443daa799974a2a6571df50b3dc9c05
shriii19 <mudabeshrinivas@gmail.com>
2026-09-02 21:07:15 +0530

    05.Add missing directory creation for REPORTS_DIR in config.py.

--- 05-CSV Data Analyzer/config.py
+++ 05-CSV Data Analyzer/config.py
@@ -2,10 +2,12 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent
 DATA_DIR = BASE_DIR / "data"
+REPORTS_DIR = BASE_DIR / "reports"
 LOG_DIR = BASE_DIR / "logs"
 
 EMPLOYEES_FILE = DATA_DIR / "employees.csv"
 LOG_FILE = LOG_DIR / "csv_analyzer.log"
 
 DATA_DIR.mkdir(parents=True, exist_ok=True)
+REPORTS_DIR.mkdir(parents=True, exist_ok=True)
 LOG_DIR.mkdir(parents=True, exist_ok=True)
